4f47548938746f09be0c758e3ba651521ce8df08
Henzo Gomes <hmarinho@its.jnj.com>
2023-04-04 17:56:02 -0300

    config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from 'path';
+import indexRouter from "./routes/index";
+import createError from "http-errors";
 
 const app = express();
 dotenv.config(); //Reads .env file and makes it accessible via process.env
 
-app.get("/test", (req: Request, res: Response, next: NextFunction) => {
-  res.send("hi!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
 app.listen(process.env.PORT, () => {
